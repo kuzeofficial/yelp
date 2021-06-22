d7fd7b29b966817e1c58cea51b97807669a9905d
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-21 21:48:54 -0700

    Started build TopNavBar

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopNav from './TopNav/TopNav'
+
+export default function LandingPage() {
+    return (
+        <div>
+            <TopNav />
+        </div>
+    )
+}
