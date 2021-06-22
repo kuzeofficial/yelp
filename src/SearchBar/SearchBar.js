4b0f2202f06d3748c96eaebb65b8a85a8e482c4e
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-22 11:01:47 -0700

    Added Search Bar

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from "./SearchBar.module.css"
+
+export  default function SearchBar () {
+    return (
+       <div>
+           <div className="field has-addons ">
+                <p className="control">
+                    <button className="button is-static is-medium">Find</button>
+                </p>
+                <p className="control">
+                    <input className={`input is-medium  ${styles['input-control']}`} type="text" placeholder="plumbers, delivery, takeouts..." />
+                </p>
+                <p className="control">
+                    <a className="button is-static is-medium">Near</a>
+                </p>
+                <p className="control">
+                    <input className={`input is-medium  ${styles['input-control']}`} type="text" value="San Francisco, CA" />
+                </p>
+            </div>
+       </div>
+    )
+}
