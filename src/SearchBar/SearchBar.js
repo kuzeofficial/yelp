f5b59c9d02ff82191cba87d178c1d4f4ab932653
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-22 15:43:45 -0700

    Added button of search

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -12,11 +12,14 @@ export  default function SearchBar () {
                     <input className={`input is-medium  ${styles['input-control']}`} type="text" placeholder="plumbers, delivery, takeouts..." />
                 </p>
                 <p className="control">
-                    <a className="button is-static is-medium">Near</a>
+                    <a className="button is-static is-medium" >Near</a>
                 </p>
                 <p className="control">
                     <input className={`input is-medium  ${styles['input-control']}`} type="text" value="San Francisco, CA" />
                 </p>
+                <div className={`button is-medium ${styles['search-button']}`}>
+                    <span className="icon is-small"><i class="fas fa-search"></i></span>
+                </div>
             </div>
        </div>
     )
