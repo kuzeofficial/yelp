5ac327399a8c0e142795a796141d56d2356646ac
Cristian Fonseca <cristiancomas@hotmail.com>
2021-06-24 10:40:10 -0700

    Navbar Added in route of component Search

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import styles from './NavBar.module.css'
+import SearchBar from '../LandingPage/SearchBar/SearchBar'
+
+export default function NavBar() {
+    return (
+        <div className={styles['nav-bar']}>
+            <img src={logo} alt='yelp logo' className={styles.logo}></img>
+            <SearchBar/>
+            <button className={`button ${styles['nav-button']}`}>Sign in</button>
+            <button className={`button ${styles['nav-button']}`}>Register</button>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css'
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter><App /></BrowserRouter>
